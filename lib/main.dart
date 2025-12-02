9f7eb818e2fc1cf7eb4d158ceb2a49bc503f677f
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-12-03 03:20:34 +0600

    02-flutter app-all about widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: Text("Hello SOHAN"),));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Placeholder(),
-        ),
-      ),
-    );
-  }
-}
