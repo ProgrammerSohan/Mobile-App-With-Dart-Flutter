92886e2e97bfec3291635621f9054c8031912259
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-12-04 03:18:33 +0600

    04-custom class-stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-   const MaterialApp(
-      home: Scaffold(body: Center(child: Text("hello Programmer Sohan"))),
-    ),
+      const MyApp()
   );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  MaterialApp(
+      home: Scaffold(body: Center(child: Text("Hello Programmer Sohan11"))),
+    );
+  }
 }
