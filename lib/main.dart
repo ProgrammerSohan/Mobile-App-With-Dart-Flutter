1220f9ec86baf7e112475556d3857d955340c5e1
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-12-06 03:41:14 +0600

    07-safearea widget in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text(
-            "Sohan Home Page",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 25,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          centerTitle: true,
-          leading: Icon(
-            Icons.access_time_filled,
-            color: Colors.white,
-            size: 50,
-          ),
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add_a_photo, color: Colors.white, size: 50),
-            ),
-          ],
-        ),
-        body: Center(
-          child: Text(
-            "Hello Sohan",
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
-              backgroundColor: Colors.green,
-              color: Colors.red,
+      home:Scaffold (
+        backgroundColor: Colors.blueAccent,
+        body:SafeArea(
+          child:Center(
+            child: Text(
+              "Hello Sohan",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                backgroundColor: Colors.green,
+                color: Colors.red,
+              ),
             ),
           ),
         ),
