8a0a9c19bd11003b38d17c33b32c6eca3784a72e
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-12-05 08:32:17 +0600

    05-appbar design in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      const MyApp()
-  );
+  runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
-      home: Scaffold(body: Center(child: Text("Hello Programmer Sohan11"))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("Sohan Home Page"),
+          centerTitle: true,
+          leading: Icon(
+            Icons.access_time_filled,
+            color: Colors.white,
+            size: 50,
+          ),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                    Icons.add_a_photo,
+                    color: Colors.white,
+                    size: 50,
+
+                ),),
+          ],
+        ),
+        body: Center(child: Text("Hello Programmer Sohan11")),
+      ),
     );
   }
 }
